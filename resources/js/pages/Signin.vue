3fe6bcc00d04cb95ed9d0a8e1625d19f2f07f8e1
kaiser-coder <miarintsoajohary@yahoo.com>
2022-04-17 16:43:49 +0300

    fea: add message alert for signin form

--- resources/js/pages/Signin.vue
+++ resources/js/pages/Signin.vue
@@ -1,65 +1,84 @@
 <template>
-  <v-row>
-    <v-col class="mr-auto ml-auto" lg="8">
-      <v-card :loading="isLoading" elevation="3">
-        <v-row class="ma-0">
-          <v-col class="pa-0" lg="7"><div class="full-bg"></div></v-col>
-          <v-col class="pa-0" lg="5">
-            <v-form class="pa-10" lazy-validation ref="form">
-              <v-card-title class="pl-0 mb-5 d-flex flex-column justify-center">
-                <v-img
-                  src="../img/saina.jpg"
-                  class="mb-2"
-                  height="100%"
-                ></v-img>
-                <h5>Connectez vous à votre compte</h5>
-              </v-card-title>
-              <v-card-text>
-                <v-row>
-                  <v-col class="pa-0" cols="12">
-                    <v-text-field
-                      label="Adresse email"
-                      name="email"
-                      filled
-                      dense
-                      rounded
-                      required
-                      v-model="formInputs.email"
-                      :rules="rules.email"
-                    ></v-text-field>
-                  </v-col>
-                </v-row>
-                <v-row>
-                  <v-col class="pa-0" cols="12">
-                    <v-text-field
-                      label="Mot de passe"
-                      name="password"
-                      filled
-                      dense
-                      rounded
-                      required
-                      type="password"
-                      v-model="formInputs.password"
-                      :rules="rules.password"
-                    ></v-text-field>
-                  </v-col>
-                </v-row>
-              </v-card-text>
-              <v-card-actions>
-                <v-row>
-                  <v-col cols="12">
-                    <v-btn width="100%" color="primary" @click="sign">
-                      Me connecter
-                    </v-btn>
-                  </v-col>
-                </v-row>
-              </v-card-actions>
-            </v-form>
-          </v-col>
-        </v-row>
-      </v-card>
-    </v-col>
-  </v-row>
+  <v-container>
+    <v-row>
+      <v-col class="mr-auto ml-auto" lg="8">
+        <v-card :loading="isLoading" elevation="3">
+          <v-row class="ma-0">
+            <v-col class="pa-0" lg="7"
+              ><div class="full-bg pa-10">
+                <v-alert
+                  class="alert"
+                  border="left"
+                  color="orange"
+                  dense
+                  type="warning"
+                  transition="scale-transition"
+                  :value="alert.state"
+                  dismissible
+                >
+                  {{ alert.message }}
+                </v-alert>
+              </div>
+            </v-col>
+            <v-col class="pa-0" lg="5">
+              <v-form class="pa-10" lazy-validation ref="form">
+                <v-card-title
+                  class="pl-0 mb-5 d-flex flex-column justify-center"
+                >
+                  <v-img
+                    src="../img/saina.jpg"
+                    class="mb-2"
+                    height="100%"
+                  ></v-img>
+                  <h5>Connectez vous à votre compte</h5>
+                </v-card-title>
+                <v-card-text>
+                  <v-row>
+                    <v-col class="pa-0" cols="12">
+                      <v-text-field
+                        label="Adresse email"
+                        name="email"
+                        filled
+                        dense
+                        rounded
+                        required
+                        v-model="formInputs.email"
+                        :rules="rules.email"
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                  <v-row>
+                    <v-col class="pa-0" cols="12">
+                      <v-text-field
+                        label="Mot de passe"
+                        name="password"
+                        filled
+                        dense
+                        rounded
+                        required
+                        type="password"
+                        v-model="formInputs.password"
+                        :rules="rules.password"
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-card-text>
+                <v-card-actions>
+                  <v-row>
+                    <v-col cols="12">
+                      <v-btn width="100%" color="primary" @click="sign">
+                        Me connecter
+                      </v-btn>
+                    </v-col>
+                  </v-row>
+                </v-card-actions>
+              </v-form>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -85,6 +104,10 @@ export default {
             "Le mot de passe doit comporter 04 caractères au minimum",
         ],
       },
+      alert: {
+        state: false,
+        message: "",
+      },
     };
   },
   methods: {
@@ -104,11 +127,15 @@ export default {
           .post("/api/auth", formData)
           .then((result) => {
             const { data } = result;
+
             this.$session.start();
             this.$session.set("app_token", data.token);
             this.$router.push("/dashboard");
           })
           .catch((error) => {
+            this.alert.message = "Utilisateur introuvable";
+            this.alert.state = true;
+
             console.error(error);
           })
           .finally(() => {
