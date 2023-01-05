23885937336d60ecabdf56e84d8856c56cf7d971
kaiser-coder <miarintsoajohary@yahoo.com>
2023-01-05 12:25:07 +0300

    remove inused code

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -15,42 +15,6 @@ class PatientController extends Controller
 		return (new Patient())->list();
 	}
 
-	/* public function store(Request $request)
-	{
-		$req_patient = $request->input("newPatient");
-		$req_assistant = $request->input("newAssistant");
-		$req_consultation = $request->input("newConsultation");
-		$req_treatment = $request->input("newTreatment");
-
-		$is_found = Patient::where($req_patient)->first();
-		if(!$is_found) {
-			$new_patient = Patient::create($req_patient);
-
-			$new_patient->assistant()->insert($req_assistant);
-			$new_patient->treatment()->insert($req_treatment);
-
-			$new_cause = $new_patient->cause()->create([
-				"victime"  => $req_consultation["victime"],
-				"vehicule" => $req_consultation["vehicule"],
-				"securite" => $req_consultation["securite"],
-				"autre"    => $req_consultation["autre"]
-			]);
-			$new_cause->driver()->insert([
-				"nom"      => $req_consultation["nom"],
-				"prenom"   => $req_consultation["prenom"],
-				"contact"  => $req_consultation["contact"],
-				"adresse"  => $req_consultation["adresse"],
-				"cin"      => $req_consultation["cin"],
-				"vehicule" => $req_consultation["vehicule"]
-			]);
-
-			return response()->json($new_patient, 200);
-		}  else {
-			return response()->json(["message" => "Already exist"], 409);
-		}
-
-	} */
-
 	public function store(Request $request)
 	{
 		return (new Patient)->storeNew($request);
